2401e5a11c3d1dface2289e217cbeda6859ba849
Max Gaponov <gaponovmaxev@gmail.com>
2020-12-17 15:20:42 +0300

    general scheme

--- include/earley.h
+++ include/earley.h
@@ -2,16 +2,21 @@
 #include <set>
 #include <string>
 #include <vector>
+#include <tuple>
 
 struct Rule {
     char from;
     std::string to;
 };
 
+bool operator<(const Rule& lhs, const Rule& rhs);
+
 struct Configuration {
     Rule rule;
     int dot_position;
 };
+
+bool operator<(const Configuration& lhs, const Configuration& rhs);
 
 class Algo {
  public:
@@ -19,11 +24,14 @@ class Algo {
 
     bool in_language();
  private:
-    void scan(int index);
-    bool predict(int index);
-    bool complete(int index);
+    void _scan(int index);
+    bool _predict(int index);
+    bool _complete(int index);
 
     std::string _word;
     std::set<Rule> _grammar;
     std::vector<std::set<Configuration>> _configurations;
+
+    const char START_S = '\0';
+    const char S = 'S';
 };
--- src/earley.cpp
+++ src/earley.cpp
@@ -1,1 +1,21 @@
 #include <earley.h>
+
+Algo::Algo(const std::set<Rule> &grammar, const std::string &word)
+: _word(word), _grammar(grammar), _configurations(word.length() + 1) {}
+
+bool Algo::in_language() {
+    _configurations[0].insert(Configuration{Rule{START_S, std::string{S}}, 0});
+    for (int index = 0; index <= _word.length(); ++index) {
+        _scan(index);
+        while (_complete(index) || _predict(index)) {}
+    }
+    return _configurations[_word.length()].count(Configuration{Rule{START_S, std::string{S}}, 1});
+}
+
+bool operator<(const Rule& lhs, const Rule& rhs) {
+    return std::tuple(lhs.from, lhs.to) < std::tuple(rhs.from, rhs.to);
+}
+
+bool operator<(const Configuration& lhs, const Configuration& rhs) {
+    return std::tuple(lhs.rule, lhs.dot_position) < std::tuple(rhs.rule, rhs.dot_position);
+}
