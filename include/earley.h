99baf19be37864dcb11ab545a4b6a4a9cde676c1
Max Gaponov <gaponovmaxev@gmail.com>
2020-12-17 15:05:31 +0300

    arch

--- include/earley.h
+++ include/earley.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <set>
+#include <string>
+#include <vector>
+
+struct Rule {
+    char from;
+    std::string to;
+};
+
+struct Configuration {
+    Rule rule;
+    int dot_position;
+};
+
+class Algo {
+ public:
+    Algo(const std::set<Rule>& grammar, const std::string& word);
+
+    bool in_language();
+ private:
+    void scan(int index);
+    bool predict(int index);
+    bool complete(int index);
+
+    std::string _word;
+    std::set<Rule> _grammar;
+    std::vector<std::set<Configuration>> _configurations;
+};
